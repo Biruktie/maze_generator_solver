a35b153bf20329bbb508fa10a983fdd0ece50998
ABEL-357 <AbelMekuria357@gmail.com>
2026-05-15 14:32:12 +0300

    Add opposite-direction mapping

--- Direction.py
+++ Direction.py
@@ -4,3 +4,10 @@ class Direction(Enum):
     RIGHT = 1
     BOTTOM = 2
     LEFT = 3
+
+OPPOSITE = {
+    Direction.TOP: Direction.BOTTOM,
+    Direction.BOTTOM: Direction.TOP,
+    Direction.LEFT: Direction.RIGHT,
+    Direction.RIGHT: Direction.LEFT
+}
